92ff53c41f765b0d94976419722df46afc444742
mhmudDev <44923654+mhmudDev@users.noreply.github.com>
2019-06-01 17:52:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,42 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-    bot = new Discord.Client({sisableEveryone: true})
-    console.log("muuuuuuuuute,");
-bot.on('guildMemberAdd', member => {
-const mohamed= member.guild.channels.get("549012135833763845");
-if(!mohamed) return;
-if(mohamed) {
-setTimeout(() => mohamed.send(`**#~Wèlcòme To __Z__èro Sèrvèr **`), 4000)        
+var prefix = "D";
+var adminprefix = 'D'
+
+
+const developers = ["517694113190117386","517694113190117386"]
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'setg')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+     if (message.content === (adminprefix + "leave")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(adminprefix + 'setw')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'setl')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'sets')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/dream");
+      message.channel.send(`**✅**`)
+  }
+  if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith(adminprefix + 'setava')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
 }
 });
 
-bot.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
+
+client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
